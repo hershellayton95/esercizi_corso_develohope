2855dc02ec93613504138977ded6c01006eaeb33
Filippo Di Marco <filidm5795@gmail.com>
2022-09-12 13:35:22 +0200

    esercizio 1 js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function canPlay() {
+    let personName;
+    
+    if (true) {
+      personName = 'Paul';
+    }
+  
+    personName += ' plays football';
+  
+    console.log(personName);
+  }
+  
+  canPlay();
