5953a815f7f97c4453f37e08ce024ab545a981aa
Filippo Di Marco <filidm5795@gmail.com>
2022-09-25 17:27:38 +0200

    create esercizio 45

--- js/script.js
+++ js/script.js
@@ -1,33 +1,9 @@
-// come di default
-let num1 = 10;
-let num2 = 20;
-console.log('Before swap: ', num1, num2); // Before swap: 10 20
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
 
-// Variable swapping
-let temp = num2;
-num2 = num1;
-num1 = temp;
-
-console.log('After swap: ', num1, num2); // After swap: 20 10
-
-// come volete che io lo faccia
-num1 = 10;
-num2 = 20;
-console.log('Before swap: ', num1, num2); // Before swap: 10 20
-
-// spread swapping
-[num2, num1] = [num1, num2];
-
-console.log('After swap: ', num1, num2); // After swap: 20 10
-
-// metodo senza usare variabili temporanee
-num1 = 10;
-num2 = 20;
-console.log('Before swap: ', num1, num2); // Before swap: 10 20
-
-// XOR swap algorithm
-num1 = num1 + num2;
-num2 = num1 - num2;
-num1 = num1 - num2;
-
-console.log('After swap: ', num1, num2); // After swap: 20 10
+let {id, firstName, lastName, age} = person;
+console.log(id, firstName, lastName, age);
