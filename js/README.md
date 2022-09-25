2c1fb4b066a59afd7c64ab39e2618a737f1b721c
Filippo Di Marco <filidm5795@gmail.com>
2022-09-25 19:46:55 +0200

    create esercizio 56

--- js/README.md
+++ js/README.md
@@ -1,2 +1,2 @@
-# Rest parameters and spread syntax - Exercise 3
-Come mergiare il valore di `newNumber` all'interno di `numberStore` senza utilizzare il metodo `push`?
+# Rest parameters and spread syntax - Exercise 4
+Come migliorare il codice affinché si eviti di specificare ad uno a uno i valori da passare alla funzione `sum`?
--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-let numberStore = [0, 1, 2];
-let newNumber = 3;
-numberStore = [...numberStore, newNumber];
-console.log(numberStore);
+function sum(...num) {
+  return num.reduce((x, y) => {return x = x + y});
+}
+
+const numbers = [1, 2, 3];
+console.log(sum(...numbers));
+
+//anche se, a questo punto:
+
+function sum1(num) {
+  return num.reduce((x, y) => {return x = x + y});
+}
+
+const numbers1 = [1, 2, 3];
+console.log(sum1(numbers1));
