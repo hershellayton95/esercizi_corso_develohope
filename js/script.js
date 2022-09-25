4967b23be775faa7c6e103483a0433789d2d0151
Filippo Di Marco <filidm5795@gmail.com>
2022-09-25 17:59:25 +0200

    create esercizio 49

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-const person = {
-  id: 1,
-  firstName: 'Mario',
-  lastName: 'Rossi',
-  age: 25
-};
+class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+}
 
-const { id, firstName: name, lastName: surname, age: old } = person;
-console.log(id, name, surname, old);
+const developer = new Person(1, 'Mario', 'Rossi', 25);
+// Print developer as json object
+console.log(JSON.stringify(developer));
