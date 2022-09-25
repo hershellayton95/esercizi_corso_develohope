ca74bd26a23e7ba1bf691cf1ae18fcd061a11330
Filippo Di Marco <filidm5795@gmail.com>
2022-09-25 18:06:30 +0200

    create esercizio 50

--- js/script.js
+++ js/script.js
@@ -5,8 +5,16 @@ class Person {
     this.lastName = lastName;
     this.age = age;
   }
+
+  toJson() {
+    return JSON.stringify(this);
+  }
+
+  static fromJson(json) {
+    return JSON.parse(json);
+  }
 }
 
-const developer = new Person(1, 'Mario', 'Rossi', 25);
-// Print developer as json object
-console.log(JSON.stringify(developer));
+const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
+const developer = Person.fromJson(json);
+console.log(developer);
